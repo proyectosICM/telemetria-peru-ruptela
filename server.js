8ace9f2cf459a3bfe557c9c67e47b4cc88659e4c
Eduardo Aguilar <es123y123@gmail.com>
2025-02-03 17:26:06 -0500

    Primer commit

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ server.on('connection', (conn) => {
         const res = process(data);
         if (!res.error) {
             //do something with res.data
-
+            console.log('Processed data:', res.data);
             //return acknowledgement
             conn.write(res.ack);
         } else {
             // Si no hubo error, hacer algo con los datos
-            console.log('Processed data:', res.data);
-            console.log('Processed data:', res);
-            console.log(res);
+            //console.log('Processed data:', res.data);
+            //console.log('Processed data:', res);
+            //console.log(res); 
             // Regresar el reconocimiento
             //conn.write(res.ack);
         }
