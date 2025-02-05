811ca45027df558142a1a6f6a9903f62058c4c99
Eduardo Aguilar <es123y123@gmail.com>
2025-02-05 15:38:43 -0500

    c

--- server.js
+++ server.js
@@ -5,39 +5,18 @@ server.on('connection', (conn) => {
     const addr = conn.remoteAddress + ':' + conn.remotePort;
     console.log('New connection from %s', addr);
 
-    //const confirmationMessage = Buffer.from([0x01]);  // Mensaje de confirmación en formato binario
-    //conn.write(confirmationMessage);
-
     conn.on('data', (data) => {
-        //console.log('New data from connection %s: %j', addr, data);
-        //console.log(`Received packet size: ${packetSizeBytes} bytes (${packetSizeBits} bits)`);
-
-        // Recortar los dos primeros bytes
-        /*
-        if (data.length < 2) {
-            console.log('Received data is too short to process.');
-            return;
-        }*/
-
-        const lengthBytes = data.slice(0, 2); // Los dos primeros bytes que representan la longitud
-        const actualData = data.slice(2);    // El resto del paquete después de los primeros dos bytes
-
-
+        console.log('New data from connection %s: %j', addr, data);
         const res = process(data);
+        console.log('Response to connection %s: %j', addr, res);
         if (!res.error) {
             //do something with res.data
-            //console.log('Processed data:', res.data);
+            console.log('Procesado correctamente');
             //return acknowledgement
-            //conn.write(res.ack);
-            console.log("Bien")
+            conn.write(res.ack);
         } else {
-            // Si no hubo error, hacer algo con los datos
-            //console.log('Processed data:', res.data);
-            //console.log('Processed data:', res);
-            //console.log(res);
-            // Regresar el reconocimiento
-            //conn.write(res.ack);
-            console.log("Error")
+            console.log('Procesado con errores');
+            //do something with res.error
         }
     });
     conn.once('close', () => {
@@ -48,8 +27,6 @@ server.on('connection', (conn) => {
     });
 });
 //configure server to listen on PORT
-const port = 9527;
-
-server.listen(port, () => {
+server.listen(PORT, () => {
     console.log('Server started on port %s at %s', server.address().port, server.address().address);
 });
