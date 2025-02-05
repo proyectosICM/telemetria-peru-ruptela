38e7f7fe53baa308cc871a34c3a8504440dedd5e
Eduardo Aguilar <es123y123@gmail.com>
2025-02-05 16:34:30 -0500

    c

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ server.on('connection', (conn) => {
 
     conn.on('data', (data) => {
         console.log('New data from connection %s: %j', addr, data);
+        console.log("Data ", data.data)
         const res = process(data);
         console.log('Response to connection %s: %j', addr, res);
         if (!res.error) {
