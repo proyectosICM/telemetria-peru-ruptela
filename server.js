127186a69df032bb0d25943206c3b7a77097885e
Eduardo Aguilar <es123y123@gmail.com>
2025-02-05 17:36:47 -0500

    c

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ server.on('connection', (conn) => {
 
     conn.on('data', (data) => {
         console.log('New data from connection %s: %j', addr, data);
-        console.log("Data ", data.type);
-        console.log("Data ", data.data);
         const res = process(data);
+
+        // Convertir a Buffer
+        const buffer = Buffer.from(data);
+
+        // Convertir a hexadecimal
+        const hexData = buffer.toString('hex');
+        console.log('Hexadecimal data:', hexData);
+
         console.log('Response to connection %s: %j', addr, res);
         if (!res.error) {
             //do something with res.data
@@ -28,7 +34,7 @@ server.on('connection', (conn) => {
         console.log('Error from connection %s: %s', addr, error.message);
     });
 });
-port = 9527; 
+port = 9527;
 //configure server to listen on PORT
 server.listen(port, () => {
     console.log('Server started on port %s at %s', server.address().port, server.address().address);
